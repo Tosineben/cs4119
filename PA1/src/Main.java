68d3fd8cbde43b8bbba991a3d3e867fb3db75b68
Alden Quimby <aldenquimby@gmail.com>
2013-02-21 23:36:25 -0500

    simple io;

--- PA1/src/Main.java
+++ PA1/src/Main.java
@@ -1,6 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("Enter your name: ");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String userName = null;
+
+        try {
+            userName = br.readLine();
+        } catch (IOException ioe) {
+            System.out.println("IO error trying to read your name!");
+            System.exit(1);
+        }
+
+        System.out.println("Thanks for the name, " + userName);
+
     }
 }
