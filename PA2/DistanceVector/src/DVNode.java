b08469d03a0d63e680cf87579742106575637435
Alden Quimby <aldenquimby@gmail.com>
2013-04-11 21:23:45 -0400

    ugh this is going to suck...

--- PA2/DistanceVector/src/DVNode.java
+++ PA2/DistanceVector/src/DVNode.java
@@ -1,4 +1,6 @@
-import java.util.ArrayList;
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
 public class DVNode {
 
@@ -28,6 +30,8 @@ public class DVNode {
             args[6] = "0.9";
         }
 
+        HashMap<Integer, Double> neighborWeights = new HashMap<Integer, Double>();
+
         int port;                       // listen on this port
         ArrayList<Neighbor> neighbors;  // these are my neighbors
         boolean last;                   // if last, i complete graph, so start broadcast
@@ -42,6 +46,7 @@ public class DVNode {
                 int nPort = Integer.parseInt(args[i]);
                 double nWeight = Double.parseDouble(args[i + 1]);
                 neighbors.add(new Neighbor(nPort, nWeight));
+                neighborWeights.put(nPort, nWeight);
             }
 
             last = "last".equals(args[args.length - 1]);
@@ -53,6 +58,53 @@ public class DVNode {
 
 
 
+    }
+
+    private static void PrintSendMessage(int nodeFromListenPort, int nodeToListenPort) {
+        long timestamp = Calendar.getInstance().getTimeInMillis();
+        String toPrint = timestamp + " Message sent from Node " + nodeFromListenPort + " to Node " + nodeToListenPort;
+        System.out.println(toPrint);
+    }
+
+    private static void PrintRcvMessage(int nodeReceivedListenPort, int nodeFromListPort) {
+        long timestamp = Calendar.getInstance().getTimeInMillis();
+        String toPrint = timestamp + " Message received at Node " + nodeReceivedListenPort + " from Node " + nodeFromListPort;
+        System.out.println(toPrint);
+    }
+
+    // TODO figure out how to store DV table...
+    private static void PrintRoutingTable(int nodePort, ArrayList<Neighbor> neighbors, HashMap<String, Neighbor> otherDudes) {
+        long timestamp = Calendar.getInstance().getTimeInMillis();
+        String toPrint = timestamp + " Node " + nodePort + " - Routing Table";
+        for (Neighbor n : neighbors) {
+            // TODO: round weights to 3 decimal places
+            toPrint += "\nNode " + n.Port + " -> (" + n.Weight + " )";
+        }
+        for (String port : otherDudes.keySet()) {
+            Neighbor next = otherDudes.get(port);
+            toPrint += "\nNode " + port + " [next " + next.Port + "] -> (" + next.Weight + ")";
+        }
+        System.out.println(toPrint);
+    }
+
+
+    private static void UnreliableSend(DatagramSocket senderSocket, int toPort, String message) throws IOException {
+        // all communication is on the same machine, so use local host
+        InetAddress receiverAddress = InetAddress.getLocalHost();
+        byte[] buffer = message.getBytes();
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length, receiverAddress, toPort);
+        senderSocket.send(packet);
+    }
+
+    public static String UnreliableReceive(DatagramSocket receiverSocket) throws IOException {
+        byte[] buffer = new byte[1024];
+        DatagramPacket receiverPacket = new DatagramPacket(buffer, buffer.length);
+        receiverSocket.receive(receiverPacket);
+        String fromIP = receiverPacket.getAddress().getHostAddress();
+        int fromPort = receiverPacket.getPort();
+        // TODO return port
+        String msg = new String(buffer, 0, receiverPacket.getLength()).trim();
+        return msg;
     }
 
 }
